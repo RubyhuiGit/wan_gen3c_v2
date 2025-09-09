b74be1412a8b28a29bc13f482f1e76fe5906e0c7
rubyhui.mo <rubyhui.mo@gmail.com>
2025-09-09 18:37:32 +0800

    init project

---
+++ run_debug.sh
@@ -0,0 +1,16 @@
+accelerate launch examples/wanvideo/model_training/train.py \
+  --dataset_base_path /root/autodl-tmp/all_datasets \
+  --dataset_metadata_path /root/autodl-tmp/all_datasets/metadata.csv \
+  --data_file_keys "video,vace_video,vace_reference_image" \
+  --height 480 \
+  --width 832 \
+  --num_frames 49 \
+  --dataset_repeat 100 \
+  --model_id_with_origin_paths "Wan-AI/Wan2.1-VACE-1.3B:diffusion_pytorch_model*.safetensors,Wan-AI/Wan2.1-VACE-1.3B:models_t5_umt5-xxl-enc-bf16.pth,Wan-AI/Wan2.1-VACE-1.3B:Wan2.1_VAE.pth" \
+  --learning_rate 1e-4 \
+  --num_epochs 2 \
+  --remove_prefix_in_ckpt "pipe.vace." \
+  --output_path "/root/autodl-tmp/output" \
+  --trainable_models "vace" \
+  --extra_inputs "vace_video" \
+  --use_gradient_checkpointing_offload
